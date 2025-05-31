c2454d37cbb0c9d93d922dec2a2b8f9d0be297dd
Ximena Burbano <daburbanoo@unal.edu.co>
2025-05-31 09:13:05 -0500

    test

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 #mlflow run https://github.com/jdvelasq/mlflow-wine-quality.git -e knn -P n_neighbors=5
-mlflow run https://github.com/daxiburbano/mlflow-wine-quality.git -e knn -P n_neighbors=5
+mlflow run https://github.com/daxiburbano/PRE-10-mlflow-ambiente-docker-daxiburbano -e knn -P n_neighbors=5
